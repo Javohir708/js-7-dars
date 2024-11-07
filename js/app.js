be3a71b5739f2c5efb3e8ebb2301c1499d5d52ca
JoxaUzKing <joxauzking@gmail.com>
2024-11-07 22:45:51 +0500

    tayyor

--- js/app.js
+++ js/app.js
@@ -6,3 +6,32 @@ window.addEventListener('scroll', function() {
         navbar.style.backgroundColor = 'white';
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navbar = document.querySelector('.navbar');
+    const navLinks = document.querySelectorAll('.navbar__link');
+    
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); 
+            
+            const targetID = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetID);
+            
+            if (targetID === '') {
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                });
+                return;
+            }
+            
+            const navbarHeight = navbar.offsetHeight;
+
+            window.scrollTo({
+                top: targetSection.offsetTop - navbarHeight,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
